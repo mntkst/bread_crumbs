f740e11fd09f19bf83a1eb9dff55397dc7fa6692
mntkst <munetaka.saito@gmail.com>
2025-07-22 00:51:26 +0900

    更新

--- lib/bread_crumbs/breadcrumbs_helper.rb
+++ lib/bread_crumbs/breadcrumbs_helper.rb
@@ -7,7 +7,6 @@ module BreadCrumbs
             return ''.html_safe if categories_with_keys.nil? || categories_with_keys.empty?
   
             breadcrumbs_html = []
-            breadcrumbs_data = nil
   
             # 各カテゴリごとのパンくずリストと構造化データを生成
             categories_with_keys.each do |category, key|
@@ -15,20 +14,17 @@ module BreadCrumbs
 
                 # HTML用パンくずリスト
                 breadcrumbs_html << render_breadcrumbs_for_category(breadcrumbs, options[:class] || 'breadcrumbs', options[:separator] || ' 》 ')
-
-                # 構造化データ用パンくずリスト(ld+jsonには1セットしか推奨されないため最初の1セットだけを表示)
-                breadcrumbs_data = generate_ld_json_breadcrumbs(breadcrumbs) if breadcrumbs_data.nil?
             end               
             # 全体を囲む要素でラップして返す
-            render_breadcrumbs_wrapper(breadcrumbs_html, breadcrumbs_data, options)
+            render_breadcrumbs_wrapper(breadcrumbs_html, options)
         end        
         private
   
-        def render_breadcrumbs_wrapper(breadcrumbs_html, breadcrumbs_data, options)
+        def render_breadcrumbs_wrapper(breadcrumbs_html, options)
             content_tag(:div, class: options[:wrapper_class] || 'breadcrumbs-wrapper') do
-                breadcrumbs_html.join("\n").html_safe +
-                render_ld_json(breadcrumbs_data)
+                breadcrumbs_html.join("\n").html_safe
             end
+
         end        
         def render_breadcrumbs_for_category(breadcrumbs, nav_class, separator)
             content_tag(:nav, class: nav_class) do
@@ -37,31 +33,6 @@ module BreadCrumbs
                 end.join(separator).html_safe
             end
         end
-
-        def generate_ld_json_breadcrumbs(breadcrumbs)
-            list = BreadCrumbs::List.instance
-
-            {
-                "@context": "https://schema.org",
-                "@type": "BreadcrumbList",
-                "itemListElement": breadcrumbs.each_with_index.map do |crumb, index|
-                    {
-                    "@type": "ListItem",
-                    "position": index + 1,
-                    "name": crumb[:title],
-                    "item": to_absolute_url(crumb[:url])
-                    }
-                end
-            }
-        end        
-        def to_absolute_url(relative_url)
-            URI.join(request.base_url, relative_url).to_s
-        end        
-        def render_ld_json(ld_json_data)
-            content_tag(:script, type: "application/ld+json") do
-                ld_json_data.to_json.html_safe
-            end
-        end   
     end
 end
   
